a30cd16f7af199177aecf5e766821bf594a12c0b
Jheisson-santos <155785645+Jheisson-santos@users.noreply.github.com>
2024-09-11 20:27:19 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoDB = "mongodb+srv://Jheisson_Santos:JSON_EU@lionscurso.6svtk.mongodb.net/petshop"
+const mongoDB = "mongodb+srv://****************.6svtk.mongodb.net/petshop"
 
 async function main() {
     await mongoose.connect(mongoDB);
